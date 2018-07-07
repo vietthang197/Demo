d2369d0dfe642ec336b8ba7a74726656ece2bb51
Le Viet Thang <vtcompact97@gmail.com>
2018-07-08 00:40:34 +0700

    big update mini

--- src/main/java/com/example/demo/filter/JWTLoginFilter.java
+++ src/main/java/com/example/demo/filter/JWTLoginFilter.java
@@ -15,6 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collections;
 
 public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
@@ -51,9 +52,12 @@ public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
     }
 
     @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse res, AuthenticationException failed) throws IOException, ServletException {
         //super.unsuccessfulAuthentication(request, response, failed);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setHeader("Authorization", "");
+        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter printWriter = res.getWriter();
+        printWriter.write("{\"authorization\":"+null+"}");
+        printWriter.flush();
+        printWriter.close();
     }
 }
